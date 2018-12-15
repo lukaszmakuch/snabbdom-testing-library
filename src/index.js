98ad10a055f9dd309b3f2d603014d8ab4d122067
Łukasz Makuch <zopsesen@gmail.com>
2018-12-15 14:10:55 +0100

    Separated the root element from the container.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ export * from 'dom-testing-library';
 import {within} from 'dom-testing-library';
 
 export const makeRender = ({patch}) => vnode => {
-  const container = document.createElement('div');
-  patch(container, vnode);
+  let container = document.createElement('div');
+  let rootElement = document.createElement('div');
+  container.appendChild(rootElement);
+  patch(rootElement, vnode);
   return {
     container,
     ...within(container),
